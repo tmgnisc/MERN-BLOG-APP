caacf50fc13c436cbd66e689714f9a91e3dd4dc6
Nischal <tamangnischal2018@gmail.com>
2025-01-22 18:11:05 +0545

    react setup and tailwind css with folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className='text-red-500 text-5xl'>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
